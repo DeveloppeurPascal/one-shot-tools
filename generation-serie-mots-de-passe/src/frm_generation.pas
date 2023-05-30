b73ef4d02072c31ed911f065e8db9debe7df93d5
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-05-30 18:47:11 +0200

    updated password generation for stronger passwords by default (12 to 18 x A-Za-z0-9 chars)

--- generation-serie-mots-de-passe/src/frm_generation.pas
+++ generation-serie-mots-de-passe/src/frm_generation.pas
@@ -3,7 +3,8 @@ unit frm_generation;
 interface
 
 uses
-  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  System.SysUtils, System.Types, System.UITypes, System.Classes,
+  System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
   FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.Memo.Types;
 
@@ -28,61 +29,71 @@ implementation
 
 function serie_aleatoire: string;
 var
-  ch : string;
-  i : integer;
-  n : integer;
+  i: integer;
+  c: char;
+  choix: integer;
 begin
-  ch := '';
-  for i := 1 to 8 do begin
-    n := random (26+10);
-    if (n < 26) then
-      ch := ch + chr (ord ('a')+n)
-    else
-      ch := ch + chr (ord ('0')+n-26);
-    {endif}
+  result := '';
+  for i := 1 to 12 + random(7) do
+  begin
+    choix := random(26 + 26 + 10);
+    case choix of
+      0 .. 25:
+        c := chr(ord('a') + choix);
+      26 .. 51:
+        c := chr(ord('A') + choix - 26);
+      52 .. 61:
+        c := chr(ord('0') + choix - 52);
+    end;
+    result := result + c;
   end;
-  Result := ch;
 end;
 
-function no_space (ch : string) : string;
+function no_space(ch: string): string;
 var
-  n : integer;
+  n: integer;
 begin
   repeat
-    n := pos (' ', ch);
+    n := pos(' ', ch);
     if (n > 0) then
-      delete (ch, n, 1);
-    {endif}
+      delete(ch, n, 1);
+    { endif }
   until (n = 0);
-  Result := ch;
+  result := ch;
 end;
 
 procedure TForm2.Button1Click(Sender: TObject);
 var
-  i : integer;
-  n : integer;
-  ch1, ch2, ch : string;
+  i: integer;
+  n: integer;
+  ch1, ch2, ch: string;
 begin
-  for i := 0 to Memo1.Lines.Count-1 do begin
-    ch := memo1.lines[i];
-    n := pos (#9, ch);
-    if (n > 0) then begin
-      ch1 := no_space (trim (LowerCase (copy (ch, 1, pred (n)))));
-      ch2 := copy (ch, succ (n), length (ch));
+  for i := 0 to Memo1.Lines.Count - 1 do
+  begin
+    ch := Memo1.Lines[i];
+    n := pos(#9, ch);
+    if (n > 0) then
+    begin
+      ch1 := no_space(trim(LowerCase(copy(ch, 1, pred(n)))));
+      ch2 := copy(ch, succ(n), length(ch));
       if (ch2 = '') then
         ch2 := serie_aleatoire;
-      {endif}
-    end else begin
-      ch1 := no_space (trim (LowerCase (ch)));
+      { endif }
+    end
+    else
+    begin
+      ch1 := no_space(trim(LowerCase(ch)));
       ch2 := serie_aleatoire;
     end;
-    memo1.lines[i] := ch1+#9+ch2;
+    Memo1.Lines[i] := ch1 + #9 + ch2;
   end;
-  memo1.SelectAll;
-  memo1.CopyToClipboard;
-  showmessage ('liste copiée dans le presse papier');
+  Memo1.SelectAll;
+  Memo1.CopyToClipboard;
+  showmessage('liste copiée dans le presse papier');
 end;
 
 initialization
-  randomize;
+
+randomize;
+
 end.
