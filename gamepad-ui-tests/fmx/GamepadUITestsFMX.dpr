6fccd5b509453387ac46babfae04c93ba00d3d87
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-07-16 13:43:24 +0200

    added a FireMonkey gamepad test project

---
+++ gamepad-ui-tests/fmx/GamepadUITestsFMX.dpr
@@ -0,0 +1,21 @@
+program GamepadUITestsFMX;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fMainFMX in 'fMainFMX.pas' {Form1},
+  uJoystickManager in '..\uJoystickManager.pas',
+  Gamolf.FMX.Joystick in '..\..\lib-externes\Delphi-Game-Engine\src\Gamolf.FMX.Joystick.pas',
+  Gamolf.RTL.Joystick.DirectInput.Win in '..\..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.DirectInput.Win.pas',
+  Gamolf.RTL.Joystick.Mac in '..\..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.Mac.pas',
+  Gamolf.RTL.Joystick in '..\..\lib-externes\Delphi-Game-Engine\src\Gamolf.RTL.Joystick.pas',
+  iOSapi.GameController in '..\..\lib-externes\Delphi-Game-Engine\src\iOSapi.GameController.pas',
+  Macapi.GameController in '..\..\lib-externes\Delphi-Game-Engine\src\Macapi.GameController.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
