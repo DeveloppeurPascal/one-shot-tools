e7039581b5c8d8aa1e54b3113e07fd605ad099e7
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-12-10 19:54:48 +0100

    added a simple project to fix a typo in generated websites and add title pictures where none is available

---
+++ GenSite2025-FixFooterAndLinks/GenSite2025FixFooterAndLinksProject.dpr
@@ -0,0 +1,56 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  One Shot Tools
+
+  Copyright 2022-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://oneshottools.developpeur-pascal.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/one-shot-tools
+
+  ***************************************************************************
+  File last update : 2025-12-10T18:29:34.000+01:00
+  Signature : 53e9c6d106792e6881100ca685c91ce31a57d577
+  ***************************************************************************
+*)
+
+program GenSite2025FixFooterAndLinksProject;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  fMain in 'fMain.pas' {frmMain},
+  Olf.FMX.SelectDirectory in '..\lib-externes\Delphi-FMXExtend-Library\src\Olf.FMX.SelectDirectory.pas',
+  udmAdobeStock_186670253 in 'X:\AdobeStock_186670253\udmAdobeStock_186670253.pas' {dmAdobeStock_186670253: TDataModule},
+  udmAdobeStock_186670296 in 'X:\AdobeStock_186670296\udmAdobeStock_186670296.pas' {dmAdobeStock_186670296: TDataModule},
+  Olf.FMX.TextImageFrame in '..\lib-externes\librairies\src\Olf.FMX.TextImageFrame.pas' {OlfFMXTextImageFrame: TFrame},
+  udmAdobeStock_257148021 in 'X:\AdobeStock_257148021\udmAdobeStock_257148021.pas' {dmAdobeStock_257148021: TDataModule},
+  udmAdobeStock_310821053 in 'X:\AdobeStock_310821053\udmAdobeStock_310821053.pas' {dmAdobeStock_310821053: TDataModule};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TdmAdobeStock_186670253, dmAdobeStock_186670253);
+  Application.CreateForm(TdmAdobeStock_186670296, dmAdobeStock_186670296);
+  Application.CreateForm(TdmAdobeStock_257148021, dmAdobeStock_257148021);
+  Application.CreateForm(TdmAdobeStock_310821053, dmAdobeStock_310821053);
+  Application.Run;
+end.
---
+++ GenSite2025-FixFooterAndLinks/fMain.pas
@@ -0,0 +1,273 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  One Shot Tools
+
+  Copyright 2022-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://oneshottools.developpeur-pascal.fr/
+
+  Project site :
+  https://github.com/DeveloppeurPascal/one-shot-tools
+
+  ***************************************************************************
+  File last update : 2025-12-10T19:10:16.000+01:00
+  Signature : 416948ba035c25d76e75c03fa1e95cb401505e3c
+  ***************************************************************************
+*)
+
+unit fMain;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Types,
+  System.UITypes,
+  System.Classes,
+  System.Variants,
+  FMX.Types,
+  FMX.Controls,
+  FMX.Forms,
+  FMX.Graphics,
+  FMX.Dialogs,
+  FMX.Memo.Types,
+  FMX.ScrollBox,
+  FMX.Memo,
+  FMX.Edit,
+  FMX.StdCtrls,
+  FMX.Controls.Presentation,
+  Olf.FMX.SelectDirectory;
+
+type
+  TfrmMain = class(TForm)
+    Edit1: TEdit;
+    btnStart: TButton;
+    EllipsesEditButton1: TEllipsesEditButton;
+    Memo1: TMemo;
+    OlfSelectDirectoryDialog1: TOlfSelectDirectoryDialog;
+    procedure EllipsesEditButton1Click(Sender: TObject);
+    procedure btnStartClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+  protected
+    procedure ProcessFolder(const Path: string; const WebSites: TStringList);
+    procedure ProcessSite(const SiteName, SiteURL, Path: string);
+    procedure CreateTitleImages(const SiteTitle, SiteURL, Path: string);
+  public
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.fmx}
+
+uses
+  System.IOUtils,
+  System.JSON,
+  Olf.FMX.TextImageFrame,
+  udmAdobeStock_186670253,
+  udmAdobeStock_186670296,
+  FMX.ImgList,
+  FMX.Layouts,
+  udmAdobeStock_257148021,
+  udmAdobeStock_310821053;
+
+procedure TfrmMain.btnStartClick(Sender: TObject);
+var
+  WebSites: TStringList;
+  i: integer;
+  s: string;
+begin
+  WebSites := TStringList.Create;
+  try
+    ProcessFolder(edit1.Text, Websites);
+    if WebSites.Count > 1 then
+    begin
+      websites.Sort;
+      s := '<!DOCTYPE html><html><head><meta charset="UTF-8">' +
+      '<meta http-equiv="content-type" content="text/html; charset=UTF-8">' +
+      '<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">' +
+      '<title>Websites list</title></head><body><h1>Websites list</h1>';
+      for i := 0 to websites.Count - 1 do
+        s := s + '<p><a href="' + WebSites[i] + '">' + WebSites[i] + '</a></p>';
+      s := s + '</body></html>';
+      TFile.WriteAllText(tpath.combine(edit1.Text, 'websites.html'), s, TEncoding.UTF8);
+    end;
+  finally
+    WebSites.Free;
+  end;
+end;
+
+procedure TfrmMain.CreateTitleImages(const SiteTitle, SiteURL, Path: string);
+var
+  l: TLayout;
+  txt: TOlfFMXTextImageFrame;
+  bmp: TBitmap;
+  ImgPath: string;
+  LightFont, DarkFont: TImageList;
+begin
+  if siteurl.contains('gamolf.fr') then
+  begin
+    LightFont := dmAdobeStock_310821053.ImageList;
+    DarkFont := lightfont;
+  end
+  else if siteurl.contains('olfsoftware.fr') then
+  begin
+    LightFont := dmAdobeStock_257148021.ImageList;
+    DarkFont := lightfont;
+  end
+  else
+  begin
+    LightFont := dmAdobeStock_186670296.ImageList;
+    DarkFont := dmAdobeStock_186670253.ImageList;
+  end;
+  ImgPath := tpath.combine(path, '..', 'img');
+  if not tdirectory.Exists(imgpath) then
+  begin
+    TDirectory.CreateDirectory(ImgPath);
+    tfile.Copy(tpath.Combine(path, 'index.php'), tpath.Combine(ImgPath, 'index.php'));
+  end;
+  l := tlayout.Create(self);
+  try
+    l.parent := self;
+    l.Width := 800;
+    l.height := 250;
+    txt := TOlfFMXTextImageFrame.Create(self);
+    try
+      txt.parent := l;
+      txt.Align := TAlignLayout.Center;
+      txt.AutoSize := true;
+      txt.Text := SiteTitle;
+      // Title in light font
+      txt.height := l.height;
+      txt.Font := LightFont;
+      txt.Refresh;
+      l.height := txt.height;
+      bmp := l.MakeScreenshot; // Take current BitmapScale
+      // TODO : see how to resize the Bitmap to save it with a 1:1 BMPScale
+      l.height := 250;
+      try
+        if not tfile.Exists(tpath.combine(ImgPath, 'title-light.png')) then
+          bmp.SaveToFile(tpath.combine(ImgPath, 'title-light.png'));
+      finally
+        bmp.free;
+      end;
+      // Title in dark font
+      txt.height := l.height;
+      txt.Font := DarkFont;
+      txt.Refresh;
+      l.height := txt.height;
+      bmp := l.MakeScreenshot;
+      l.height := 250;
+      try
+        if not tfile.Exists(tpath.combine(ImgPath, 'title-dark.png')) then
+          bmp.SaveToFile(tpath.combine(ImgPath, 'title-dark.png'));
+      finally
+        bmp.free;
+      end;
+    finally
+      txt.Free;
+    end;
+  finally
+    l.Free;
+  end;
+end;
+
+procedure TfrmMain.EllipsesEditButton1Click(Sender: TObject);
+begin
+  if OlfSelectDirectoryDialog1.Execute then
+    Edit1.Text := OlfSelectDirectoryDialog1.Directory;
+end;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+{$IFDEF MSWINDOWS}
+  edit1.Text := 'C:\xampp\htdocs\_____AllProjectsWebSites';
+{$ENDIF}
+{$IFDEF OSX}
+  edit1.Text := '/Users/patrickpremartin/Sites/_FTPSauron';
+{$ENDIF}
+end;
+
+procedure TfrmMain.ProcessFolder(const Path: string; const WebSites: TStringList);
+var
+  FolderName, DomainName: string;
+  Folders: TStringDynArray;
+  i: integer;
+begin
+  if (not path.IsEmpty) and tdirectory.Exists(path) then
+  begin
+    FolderName := tpath.GetFileName(path);
+    if (FolderName.StartsWith('_db-') and tfile.Exists(tpath.Combine(path, 'settings.json'))) then
+    begin
+      DomainName := tpath.GetFileName(tpath.GetDirectoryName(Path));
+      memo1.lines.add('********************************************');
+      memo1.lines.add('> https://' + DomainName);
+      memo1.lines.add('> ' + tpath.GetDirectoryName(Path));
+      ProcessSite(DomainName, 'https://' + DomainName, Path);
+      websites.Add('https://' + DomainName);
+    end
+    else
+    begin
+      folders := tdirectory.GetDirectories(path);
+      for i := 0 to length(folders) - 1 do
+        processfolder(folders[i], WebSites);
+    end;
+  end;
+end;
+
+procedure TfrmMain.ProcessSite(const SiteName, SiteURL, Path: string);
+var
+  Settings, PrevSettings: string;
+  jso: TJSONObject;
+  jsa: TJSONArray;
+  SiteTitle: string;
+  i: integer;
+  Key, Value: string;
+begin
+  if (not path.IsEmpty) and tdirectory.Exists(path) and tfile.Exists(tpath.Combine(path, 'settings.json')) then
+  begin
+    PrevSettings := tfile.ReadAllText(tpath.Combine(path, 'settings.json'));
+    Settings := PrevSettings.Replace('Nos libraries', 'Nos librairies');
+    Settings := Settings.Replace('default_image":{}',
+      'default_image":{"content":[{"lang":"en","text":"","url_image":"/img/title-light.png","is_public":true}],"is_public":true}');
+    if not SameText(settings, PrevSettings) then
+      TFile.WriteAllText(tpath.Combine(path, 'settings.json'), settings, TEncoding.ANSI);
+    jso := TJSONObject.ParseJSONValue(settings) as TJSONObject;
+    try
+      jsa := jso.GetValue<TJSONArray>('storage');
+      if assigned(jsa) and (jsa.count > 0) then
+        for i := 0 to jsa.count - 1 do
+          if (jsa[i] as TJSONObject).TryGetValue<string>('key', key) and SameText(Key, 'site_name') and (jsa[i] as
+            TJSONObject).TryGetValue<string>('value', SiteTitle) then
+          begin
+            CreateTitleImages(SiteTitle, SiteURL, Path);
+            break;
+          end;
+    finally
+      jso.Free;
+    end;
+  end;
+end;
+
+initialization
+  ReportMemoryLeaksOnShutdown := true;
+end.
+
