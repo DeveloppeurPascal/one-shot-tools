6b9162d5f7b019a20c4e583670f85de8aef32fbe
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-12-17 21:17:42 +0100

    updated header copyright and timestamp comments

--- generation-serie-mots-de-passe/src/frm_generation.pas
+++ generation-serie-mots-de-passe/src/frm_generation.pas
@@ -2,16 +2,20 @@
   ***************************************************************************
 
   One Shot Tools
+  Copyright (c) 2022-2025 Patrick PREMARTIN
 
-  Copyright 2022-2025 Patrick PREMARTIN under AGPL 3.0 license.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Affero General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or (at your option) any later version.
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-  DEALINGS IN THE SOFTWARE.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Affero General Public License for more details.
+
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
   ***************************************************************************
 
@@ -25,7 +29,7 @@
   https://github.com/DeveloppeurPascal/one-shot-tools
 
   ***************************************************************************
-  File last update : 2025-02-09T11:12:13.651+01:00
+  File last update : 2025-07-14T09:18:14.262+02:00
   Signature : 86d7a14a2dcedc6e98af2d3cd45d6c333fa17797
   ***************************************************************************
 *)
